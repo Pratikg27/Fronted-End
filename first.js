54c08f3b2c2c176b0b7eea277f4d75a3d0d6ed0a
Pratikg27 <pratikgunjal2127@gmail.com>
2024-12-03 15:30:27 +0530

    Add files

---
+++ first.js
@@ -0,0 +1,67 @@
+// console.log("Hello JavaScript");
+
+
+/*
+let a=6;
+let b=7;
+let c=a+b;
+console.log(c)
+*/
+
+
+
+/*
+let a=7;
+let c=6;
+let b=a+(~c+1);
+console.log(b);
+*/
+
+
+/*
+let char='pratik'
+console.log(char)
+*/
+
+
+/*
+let a =48;
+if(a>=0){
+    console.log("Positive");
+}else if(a<=0){
+    console.log("Negative");
+}
+  */
+
+
+/*
+let a=28;
+if(a % 2==0){
+    console.log('even');
+}else{
+    console.log('odd')
+}*/
+
+
+
+let a=10;
+let b=20;
+
+a=a+b;
+b=a-b;
+a=a-b;
+ 
+
+// let temp=a;
+// a=b;
+// b=temp;
+
+console.log("a :" +a)
+console.log("b :" +b)
+
+
+num=100; sum=0;
+for(let i=0 ; i<=num; i++)
+    sum =sum+i;{
+console.log("sum :"+sum);
+}
